63bcca8e54d0d22ad67affda0362daecc9361b87
Ashishadhikari123 <adhikarigolu987@gmail.com>
2020-12-04 18:01:23 +0530

    nth fibonacci number using recursion

---
+++ fib_recursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n)
+{
+	if(n==0 || n==1)
+	{
+		return n;
+	}
+	return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+	int n;
+	cout<<"ENTER THE VALUE OF N"<<endl;
+	cin>>n;
+	cout<<"Fibonacci number at index "<<n<<"is "<<fib(n)<<endl;
+	return 0;
+}
