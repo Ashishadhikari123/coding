bf923695d62fe21315cb99a79bdcd1f508353910
Ashishadhikari123 <adhikarigolu987@gmail.com>
2020-12-14 15:49:03 +0530

    initialising vector and printing vector elements

---
+++ vector1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void display(vector<int>&v)
+{
+	cout<<"VECTOR ELEMENTS ARE"<<endl;
+	for(int i=0;i<v.size();i++)
+	{
+		cout<<v[i]<<" ";
+	}
+}
+
+int main()
+{
+	int n,x;
+	cout<<"ENTER SIZE"<<endl;
+	cin>>n;
+	vector<int>v;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"ENTER ELEMENT "<<i+1<<endl;
+		cin>>x;
+		v.push_back(x);
+	}
+	display(v);
+	return 0;
+}
